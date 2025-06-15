fe0ad7aca5fbb30feb411931c3335d866babce2f
sasajy <128241767+sasajy@users.noreply.github.com>
2025-06-15 23:50:35 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,21 @@
 import streamlit as st
 from collections import deque
+st.set_page_config(layout="wide")  # ← これが最重要
+
+# ページ幅CSS
+st.markdown("""
+<style>
+[data-testid="stAppViewContainer"] > .main {
+    max-width: 1600px;
+    padding-left: 2rem;
+    padding-right: 2rem;
+}
+[data-testid="stSidebar"] {
+    width: 250px;
+}
+</style>
+""", unsafe_allow_html=True)
+
 
 memory = [0] * 30000
 pointer = 0
@@ -45,9 +61,30 @@ def run_brainfuck(start,goal):
         _ += 1    
 
 
-st.title("Brainfuck Editor & Memory Viewer")
+st.title("Brainfuck Editor")
+def_input = """++++++++++[
+    >+++++++
+    >++++++++++
+    >+++++++++++
+    >+++
+    >+++++++++
+    >+
+    <<<<<<-
+]
+>++.
+>+.
+>--..
++++.
+>++.
+>---.
+<<.
++++.
+------.
+<-.
+>>+.
+>>."""
 
-code = st.text_area("Brainfuck Code", value="")
+code = st.text_area("Brainfuck Code", value=f"{def_input}")
 input_str = st.text_area("Input", "")
 run = st.button("Run")
 
@@ -75,3 +112,38 @@ if run:
 
 
     st.write(f"Pointer Position: {pointer}")
+    
+
+st.markdown("---")
+st.subheader("おまけ")
+
+# 1. コマンドマッピング入力
+st.caption("以下で各 Brainfuck コマンドに対応する記号を設定")
+
+symbol_map = {}
+default_symbols = ["新手のスタンド使いか！", "ハーミットパープル", "スターフィンガー！", "ロードローラーだ！", "オラ", "無駄", "あ…ありのまま 今　起こった事を話すぜ！", "ザ・ワールド！"]
+command_names = [",", ".", ">", "<", "+", "-", "[", "]"]
+
+cols = st.columns(8)
+for i, cmd in enumerate(command_names):
+    with cols[i]:
+        symbol = st.text_input(f"　{cmd} ", value=default_symbols[i], max_chars=3, key=f"symbol_{cmd}")
+        symbol_map[cmd] = symbol
+
+# 2. 入力テキスト
+
+cols = st.columns(2)
+
+with cols[0]:
+    st.caption("変換前")
+    custom_input = st.text_area("変換前", key="custom_input", height=100,value=code)
+
+with cols[1]:
+    # 変換処理
+    bf_code = ""
+    for i in custom_input:
+        if i in symbol_map:
+            bf_code += symbol_map[i]
+
+    st.caption("変換後")
+    st.text_area("変換後", bf_code, height=100, key="converted_output")
